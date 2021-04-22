c5c61f69b6ca05febc3c8d9d8f33f9eb94865174
muwella <marielabrascon@gmail.com>
2021-04-22 15:50:06 -0300

    Resuelvo el punto 3 del enunciado

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,35 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas / fortaleza objetivo
+
+poder :: Horas -> Number
+poder = (* 15)
+
+fortaleza :: Objetivo -> Number
+fortaleza = (2*) . length
+
+type Golpe = Objetivo -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe 
+normalesConsecutivos = presionGolpe 240
+
+objetivoDificil :: Objetivo -> Bool
+objetivoDificil = (< 100).gomuGomu
+
+focalizar :: Objetivo -> Objetivo
+focalizar objetivo = take 7 objetivo
+
+golpeFocalizado :: Objetivo -> Number
+golpeFocalizado = normalesConsecutivos.focalizar
+
+objetivoAccesible :: Objetivo -> Bool
+objetivoAccesible objetivo = (golpeFocalizado objetivo > 200) && (golpeFocalizado objetivo < 400)
